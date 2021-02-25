6fa9b144e8261d785ef87419461aaad56d3947e5
onjsdnjs <onjsdnjs@gmail.com>
2021-02-25 11:17:53 +0900

    aop test

--- src/main/java/io/anymobi/aop/demo/AopControllerInterceptor.java
+++ src/main/java/io/anymobi/aop/demo/AopControllerInterceptor.java
@@ -1,24 +1,15 @@
 package io.anymobi.aop.demo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Method;
 
-/**
- *
- * methodInvocation.proceed() 를 사용하지 않고 직접 메소드를 핸들링 해서 구현 가능함
- */
 public class AopControllerInterceptor implements MethodInterceptor {
-
-    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public Object invoke(MethodInvocation methodInvocation) throws Throwable {
 
-        // bypass
         Object[] arguments = methodInvocation.getArguments();
         HttpServletRequest request = (HttpServletRequest) arguments[0];
         MemberDto memberDto = (MemberDto) arguments[2];
@@ -31,11 +22,9 @@ public class AopControllerInterceptor implements MethodInterceptor {
             memberDto.setForm(isForm);
         }
 
-//        arguments[2] = memberDto;
-
         Object retVal = methodInvocation.proceed();
 
-        // 리플렉션을 사용해서 직접 메소드 핸들링
+        // 리플렉션을 사용해서 직접 메소드 핸들링 할 경우
         /*AopController aopController = (AopController)methodInvocation.getThis();
         Object[] arguments = methodInvocation.getArguments();
         HttpServletRequest request = (HttpServletRequest) arguments[0];
