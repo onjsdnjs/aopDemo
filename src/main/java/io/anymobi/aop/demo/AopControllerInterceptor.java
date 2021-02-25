10ed83d3d16b41aefc185eaf213a5dfcc42d117b
onjsdnjs <onjsdnjs@gmail.com>
2021-02-25 10:21:14 +0900

    aop test

--- src/main/java/io/anymobi/aop/demo/AopControllerInterceptor.java
+++ src/main/java/io/anymobi/aop/demo/AopControllerInterceptor.java
@@ -6,9 +6,6 @@ import org.aopalliance.intercept.MethodInvocation;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class AopControllerInterceptor implements MethodInterceptor {
 
@@ -22,42 +19,31 @@ public class AopControllerInterceptor implements MethodInterceptor {
         HttpServletRequest request = (HttpServletRequest) arguments[0];
         MemberDto memberDto = (MemberDto) arguments[2];
 
-        Arrays.stream(arguments).forEach(a -> System.out.println("arguments = " + a));
-
         boolean isJson = request.getRequestURI().endsWith("json");
         boolean isForm = request.getRequestURI().endsWith("form");
 
         Method[] declaredMethods = aopController.getClass().getDeclaredMethods();
-        Map<String, Method> methodMap = new HashMap<>();
-        Map<String, String> argMap = new HashMap<>();
+        Object retVal = null;
 
         for (Method method : declaredMethods) {
             String name = method.getName();
 
             if ("aopService".equals(name)) {
                 if (isJson) {
-                    methodMap.put("json", method);
-                    argMap.put("jsonArg",objectMapper.writeValueAsString(memberDto));
+                    arguments[2] = objectMapper.writeValueAsString(memberDto);
+                    retVal = method.invoke(methodInvocation.getThis(), arguments);
 
                 } else if (isForm) {
-                    methodMap.put("form", method);
                     if(memberDto != null){
-                        argMap.put("formArg",memberDto.toString());
+                        arguments[2] = memberDto.toString();
+                        retVal = method.invoke(methodInvocation.getThis(), arguments);
+                    }else{
+                        retVal = method.invoke(methodInvocation.getThis(), arguments);
                     }
                 }
             }
         }
 
-        Object retVal = null;
-        if (isJson) {
-            arguments[2] = argMap.get("jsonArg");
-            retVal = methodMap.get("json").invoke(methodInvocation.getThis(), arguments);
-
-        } else if (isForm) {
-            arguments[2] = argMap.get("formArg");
-            retVal = methodMap.get("form").invoke(methodInvocation.getThis(), arguments);
-
-        }
         return retVal;
     }
 }
